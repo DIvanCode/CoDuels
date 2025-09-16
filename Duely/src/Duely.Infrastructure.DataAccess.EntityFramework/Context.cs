4fc18c0c8690f0a572b83f9aae479091491cad34
Иван Добрынин <49978381+DIvanCode@users.noreply.github.com>
2025-09-16 22:08:29 +0300

    Created C# project for Duely (#17)

---
+++ Duely/src/Duely.Infrastructure.DataAccess.EntityFramework/Context.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using Microsoft.EntityFrameworkCore;
+
+namespace Duely.Infrastructure.DataAccess.EntityFramework;
+
+public sealed class Context : DbContext
+{
+    public Context(DbContextOptions<Context> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
